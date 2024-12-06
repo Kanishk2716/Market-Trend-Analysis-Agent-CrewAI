9f7defeb4903303d995275b268e1560f9c630bf3
Kanishk2716 <147602734+Kanishk2716@users.noreply.github.com>
2024-12-07 03:37:27 +0530

    Delete tools.py

--- tools.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
-
-from crewai_tools import SerperDevTool
-
-tool = SerperDevTool()
